0cd0c912009a76e88e5f2d9681e496aa80be4582
Shani Knobel <shani.knobel@post.runi.ac.il>
2024-02-07 23:16:40 +0200

    first commit

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -13,27 +13,45 @@ public class HashTagTokenizer {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
-
-		// Your code here
+		
+		for(int i=0; i<3000; i++)
+		{
+			dictionary[i]=in.readString();
+		}
+		
 
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		boolean exists=false;
+		for(int i=0;i<3000 && !exists; i++)
+		{
+			if(dictionary[i].equals(word))
+			{
+				exists=true;
+			}	
+		}
+		return exists;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
-
+		hashtag.toLowerCase();
 		// Base case: do nothing (return) if hashtag is an empty string.
         if (hashtag.isEmpty()) {
             return;
         }
  
         int N = hashtag.length();
+		String lowCaseHash= hashtag.toLowerCase();
 
-        for (int i = 1; i <= N; i++) {
-		
+        for (int i = 1; i <= N; i++) 
+		{
+			if(existInDictionary(lowCaseHash.substring(0, i), dictionary))
+			{
+				System.out.println(lowCaseHash.substring(0,i));
+				breakHashTag(lowCaseHash.substring(i,N), dictionary);
+			}
         }
     }
 
